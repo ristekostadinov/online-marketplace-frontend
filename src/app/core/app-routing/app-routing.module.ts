dc030e423bcbabec0feb1113531d9d8ebe0e3d57
Riste Kostadinov <ristekostadinov0@gmail.com>
2021-04-27 00:02:45 +0200

    added 3 new routes

--- src/app/core/app-routing/app-routing.module.ts
+++ src/app/core/app-routing/app-routing.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import {Routes, RouterModule} from '@angular/router'
 import { CreateCategoryComponent } from 'src/app/create-category/create-category.component';
+import { CreateTagsComponent } from 'src/app/create-tags/create-tags.component';
+import { ListTagsComponent } from 'src/app/list-tags/list-tags.component';
 import { HomePageComponent } from '../../home-page/home-page.component';
 import { ListCategoriesComponent } from '../../list-categories/list-categories.component';
 
@@ -16,6 +18,15 @@ const routes : Routes = [
   },
   {
     path: "categories/:id", component: CreateCategoryComponent
+  },
+  {
+    path: "tags", component: ListTagsComponent
+  },
+  {
+    path: "tags/create", component: CreateTagsComponent
+  },
+  {
+    path: "tags/:id", component: CreateTagsComponent
   }
 ];
 
@@ -25,4 +36,4 @@ const routes : Routes = [
   exports:[RouterModule]
 })
 export class AppRoutingModule { }
-export const routingComponents = [HomePageComponent, ListCategoriesComponent, CreateCategoryComponent]
+export const routingComponents = [HomePageComponent, ListCategoriesComponent, CreateCategoryComponent, ListTagsComponent, CreateTagsComponent]
